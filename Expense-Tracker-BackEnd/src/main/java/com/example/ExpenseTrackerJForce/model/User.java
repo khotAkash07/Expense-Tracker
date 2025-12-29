2caedf28bb12ea9af60e23c6962fc4bcb539b830
khotAkash07 <khotakash5737@gmail.com>
2025-12-29 15:50:53 +0530

    Change core security config2

--- Expense-Tracker-BackEnd/src/main/java/com/example/ExpenseTrackerJForce/model/User.java
+++ Expense-Tracker-BackEnd/src/main/java/com/example/ExpenseTrackerJForce/model/User.java
@@ -1,18 +1,23 @@
 package com.example.ExpenseTrackerJForce.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user_details")
+@Table(
+        name = "user_details",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email")
+        }
+)
 public class User {
 
     @Id
@@ -20,11 +25,20 @@ public class User {
     private Long id;
 
     private String userName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
     private String fullName;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
     @JsonIgnore
     private List<Expensee> expenses;
 }
