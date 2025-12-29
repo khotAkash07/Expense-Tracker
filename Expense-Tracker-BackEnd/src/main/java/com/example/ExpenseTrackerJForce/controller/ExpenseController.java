75ff5ed5da5e4e31846daca9b44b5f78ee402982
khotAkash07 <khotakash5737@gmail.com>
2025-12-29 15:22:50 +0530

    Remove CrossOrigin annotation from ExpenseController
    Removed CrossOrigin annotation to restrict CORS.

--- Expense-Tracker-BackEnd/src/main/java/com/example/ExpenseTrackerJForce/controller/ExpenseController.java
+++ Expense-Tracker-BackEnd/src/main/java/com/example/ExpenseTrackerJForce/controller/ExpenseController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/expense")
 public class ExpenseController {
 
